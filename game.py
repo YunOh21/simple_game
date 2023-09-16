54f471807ba3ddb69b8a762a8c9f379e4dfc420d
yoh <yun5.dev@gmail.com>
2023-09-16 18:08:20 +0900

    chore: import문 수정
    - from -> import로 수정
    - 모듈 전체 가져오고, 함수 사용 시 어느 모듈인지 알 수 있게 함

--- game.py
+++ game.py
@@ -1,14 +1,14 @@
-from fn_show_text import intro_game, goodbye
-from fn_prep import pick_your_color, set_the_enemy, set_the_order
-from fn_map import show_the_map
-from fn_play import play, show_the_winner
+import fn_show_text as show_text
+import fn_prep as prep
+import fn_map as map
+import fn_play as play
 
 
-intro_game()
-you = pick_your_color()
-enemy = set_the_enemy(you)
-show_the_map(you, enemy)
-set_the_order(you, enemy)
-play(you, enemy)
-show_the_winner(you, enemy)
-goodbye()
+show_text.intro_game()
+you = prep.pick_your_color()
+enemy = prep.set_the_enemy(you)
+map.show_the_map(you, enemy)
+prep.set_the_order(you, enemy)
+play.play(you, enemy)
+play.show_the_winner(you, enemy)
+show_text.goodbye()
